b7928567727186bba84116985a4e16e770c6f578
sourabpanch7 <panchanansourav@gmail.com>
2023-12-26 19:23:50 +0530

    Initial commit for SHAP explanations

--- spark_submit.sh
+++ spark_submit.sh
@@ -1,6 +1,6 @@
-/Users/sourabpanchanan/work/spark/spark-3.4.2-bin-hadoop3/bin/spark-submit \
+<SPARK_HOME>/spark-submit \
 --verbose \
 --master local[*] \
 --packages com.microsoft.azure:synapseml_2.12:1.0.2 \
---conf "spark.pyspark.python=/Users/sourabpanchanan/anaconda3/envs/wine_quality_pyspark_xgboost/bin/python" \
+--conf "spark.pyspark.python=<python_path>" \
 shap_spark.py
